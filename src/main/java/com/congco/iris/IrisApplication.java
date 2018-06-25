ffb77ea78dad4de191fbebc6b3d5681682d7c0cb
zhengcong <mxlyzc@163.com>
2018-06-25 16:26:46 +0800

    2

--- src/main/java/com/congco/iris/IrisApplication.java
+++ src/main/java/com/congco/iris/IrisApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IrisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IrisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IrisApplication.class, args);
+    }
 }
