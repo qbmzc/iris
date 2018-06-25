95de5adca74037705e068e6e4685411fbbd72ba8
zhengcong <mxlyzc@163.com>
2018-06-25 16:22:44 +0800

    初始化

---
+++ src/main/java/com/congco/iris/IrisApplication.java
@@ -0,0 +1,12 @@
+package com.congco.iris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrisApplication.class, args);
+	}
+}
