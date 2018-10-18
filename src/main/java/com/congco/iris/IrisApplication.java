e6af48e76b9632de8c278595711fa1523b36fff9
zhengcong <mxlyzc@163.com>
2018-10-18 12:33:01 +0800

    type(scope): 1

--- src/main/java/com/congco/iris/IrisApplication.java
+++ src/main/java/com/congco/iris/IrisApplication.java
@@ -3,6 +3,10 @@ package com.congco.iris;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author qbmzc
+ */
 @SpringBootApplication
 public class IrisApplication {
 
